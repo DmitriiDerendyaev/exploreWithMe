5d227dc989295053d0f5954ed42376aaac8cfa3b
Dmitrii Derendyaev <dds_stalker@mail.ru>
2024-04-30 22:00:31 +0400

    feature: косметические правки

--- ewm-service/src/main/java/org/example/category/model/Category.java
+++ ewm-service/src/main/java/org/example/category/model/Category.java
@@ -1,4 +1,18 @@
 package org.example.category.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private long id;
+
+    @Column(name = "category_name")
+    private String name;
 }
