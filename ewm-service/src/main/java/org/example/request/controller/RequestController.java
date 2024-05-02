967511480956abf80007cb2714ed59a5ca3b6fbf
Dmitrii Derendyaev <dds_stalker@mail.ru>
2024-05-03 01:39:27 +0400

    feature: добавить контроллер и бизнес логику для реквестов

--- ewm-service/src/main/java/org/example/request/controller/RequestController.java
+++ ewm-service/src/main/java/org/example/request/controller/RequestController.java
@@ -1,4 +1,38 @@
 package org.example.request.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.request.dto.ParticipationRequestDto;
+import org.example.request.service.RequestService;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
 public class RequestController {
+    private final RequestService requestService;
+
+    @PostMapping("/users/{userId}/requests")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ParticipationRequestDto addRequestPrivate(@PathVariable long userId,
+                                                     @RequestParam long eventId) {
+        return requestService.addRequestPrivate(userId, eventId);
+    }
+
+    @GetMapping("/users/{userId}/requests")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ParticipationRequestDto> getRequestsPrivate(@PathVariable long userId) {
+        return requestService.getRequestsPrivate(userId);
+    }
+
+    @PatchMapping("/users/{userId}/requests/{requestId}/cancel")
+    @ResponseStatus(HttpStatus.OK)
+    public ParticipationRequestDto cancelRequest(@PathVariable long userId,
+                                                 @PathVariable long requestId) {
+        return requestService.cancelRequest(userId, requestId);
+
+    }
 }
--- ewm-service/src/main/java/org/example/request/service/RequestServiceImpl.java
+++ ewm-service/src/main/java/org/example/request/service/RequestServiceImpl.java
@@ -2,10 +2,15 @@ package org.example.request.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.event.model.Event;
+import org.example.event.model.EventState;
 import org.example.event.repository.EventRepository;
 import org.example.exceprtion.ObjectNotFoundException;
+import org.example.exceprtion.RulesViolationException;
 import org.example.request.dto.ParticipationRequestDto;
 import org.example.request.mapper.RequestMapper;
+import org.example.request.model.Request;
+import org.example.request.model.RequestStatus;
 import org.example.request.repository.RequestRepository;
 import org.example.user.model.User;
 import org.example.user.repository.UserRepository;
@@ -14,7 +19,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -28,17 +35,54 @@ public class RequestServiceImpl implements RequestService {
     private final RequestMapper requestMapper;
     @Override
     public ParticipationRequestDto addRequestPrivate(Long userId, Long eventId) {
-        return null;
+        Request request = new Request();
+        Event event = eventRepository.findById(eventId).orElseThrow(
+                () -> new ObjectNotFoundException(String.format("Event with id=%d was not found", eventId)));
+        User user = getUserOrThrow(userId);
+        List<Request> requests = requestRepository.findAllByRequesterIdAndEventId(userId, eventId);
+        if (!requests.isEmpty()) {
+            throw new RulesViolationException("Your request added");
+        }
+        if (userId.equals(event.getInitiator().getId())) {
+            throw new RulesViolationException("Event's owner can't be add");
+        }
+        if (!event.getState().equals(EventState.PUBLISHED)) {
+            throw new RulesViolationException("Can't add in not published event");
+        }
+        if (event.getParticipantLimit() != 0 && event.getParticipantLimit().equals(event.getConfirmedRequests())) {
+            throw new RulesViolationException("Participant Limit");
+        }
+
+        request.setRequester(user);
+        request.setEvent(event);
+        request.setStatus(RequestStatus.PENDING);
+        request.setCreated(LocalDateTime.now());
+
+        if (event.getParticipantLimit() == 0 || !event.getRequestModeration()) {
+            request.setStatus(RequestStatus.CONFIRMED);
+            event.setConfirmedRequests(event.getConfirmedRequests() + 1);
+            eventRepository.save(event);
+        }
+        log.info("Add request by user");
+        return requestMapper.toDto(requestRepository.save(request));
     }
 
     @Override
     public List<ParticipationRequestDto> getRequestsPrivate(Long userId) {
-        return null;
-    }
+        getUserOrThrow(userId);
+        log.info("Get user requests to event");
+        return requestRepository.findAllByRequesterId(userId).stream()
+                .map(requestMapper::toDto).collect(Collectors.toList());    }
 
     @Override
     public ParticipationRequestDto cancelRequest(Long userId, Long requestId) {
-        return null;
+        getUserOrThrow(userId);
+        Request request = requestRepository.findById(requestId).orElseThrow(
+                () -> new ObjectNotFoundException(String.format("Request with id=%d not found", requestId)));
+        request.setStatus(RequestStatus.CANCELED);
+        request = requestRepository.save(request);
+        log.info("Canceled request");
+        return requestMapper.toDto(request);
     }
 
     private User getUserOrThrow(long id) {
