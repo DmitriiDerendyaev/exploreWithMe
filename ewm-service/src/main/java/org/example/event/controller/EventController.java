295d4f24a778a9a43872e5081d60a06a0ed6a7db
Dmitrii Derendyaev <dds_stalker@mail.ru>
2024-05-03 01:07:01 +0400

    feature: добавить возможность мапить запросы в контроллер

--- ewm-service/src/main/java/org/example/event/controller/EventController.java
+++ ewm-service/src/main/java/org/example/event/controller/EventController.java
@@ -1,4 +1,133 @@
 package org.example.event.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.StatsClient;
+import org.example.event.dto.*;
+import org.example.event.service.EventService;
+import org.example.request.dto.EventRequestStatusUpdateRequest;
+import org.example.request.dto.EventRequestStatusUpdateResult;
+import org.example.request.dto.ParticipationRequestDto;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.example.constant.Constants.DATE_FORMAT;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
 public class EventController {
+    private final EventService eventService;
+
+    @PostMapping("/users/{userId}/events")
+    @ResponseStatus(HttpStatus.CREATED)
+    public EventFullDto addEventPrivate(@PathVariable long userId, @Valid @RequestBody NewEventDto newEventDto) {
+        return eventService.addEventPrivate(userId, newEventDto);
+    }
+
+    @GetMapping("/users/{userId}/events")
+    @ResponseStatus(HttpStatus.OK)
+    public List<EventShortDto> getEventPrivate(@PathVariable long userId,
+                                               @RequestParam(required = false, defaultValue = "0") @Min(0) Integer from,
+                                               @RequestParam(required = false, defaultValue = "10") @Min(1) Integer size) {
+        return eventService.getEventPrivate(userId, PageRequest.of(from / size, size));
+    }
+
+    @GetMapping("/users/{userId}/events/{eventId}")
+    @ResponseStatus(HttpStatus.OK)
+    public EventFullDto getEventByIdPrivate(@PathVariable long userId,
+                                            @PathVariable long eventId) {
+        return eventService.getEventByIdPrivate(userId, eventId);
+    }
+
+    @PatchMapping("/users/{userId}/events/{eventId}")
+    @ResponseStatus(HttpStatus.OK)
+    public EventFullDto updateEventPrivate(@PathVariable long userId, @PathVariable long eventId,
+                                           @Valid @RequestBody UpdateEventUserRequest updateEvent) {
+        return eventService.updateEventPrivate(userId, eventId, updateEvent);
+    }
+
+    @GetMapping("/users/{userId}/events/{eventId}/requests")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ParticipationRequestDto> getRequestsUserToEventPrivate(@PathVariable long userId,
+                                                                       @PathVariable long eventId) {
+        return eventService.getRequestsUserToEventPrivate(userId, eventId);
+    }
+
+    @PatchMapping("/users/{userId}/events/{eventId}/requests")
+    @ResponseStatus(HttpStatus.OK)
+    public EventRequestStatusUpdateResult updateEventRequestStatusPrivate(@PathVariable long userId,
+                                                                          @PathVariable long eventId,
+                                                                          @Valid @RequestBody EventRequestStatusUpdateRequest updateRequests) {
+        return eventService.updateEventRequestStatusPrivate(userId, eventId, updateRequests);
+    }
+
+    @GetMapping("/admin/events")
+    @ResponseStatus(HttpStatus.OK)
+    public List<EventFullDto> getEventsAdmin(@RequestParam(required = false) List<Long> users,
+                                             @RequestParam(required = false) List<String> states,
+                                             @RequestParam(required = false) List<Long> categories,
+                                             @RequestParam(required = false) @DateTimeFormat(pattern = DATE_FORMAT) LocalDateTime rangeStart,
+                                             @RequestParam(required = false) @DateTimeFormat(pattern = DATE_FORMAT) LocalDateTime rangeEnd,
+                                             @RequestParam(required = false, defaultValue = "0") @Min(0) Integer from,
+                                             @RequestParam(required = false, defaultValue = "10") @Min(1) Integer size) {
+        EventAdminParam eventAdminParam = new EventAdminParam();
+        eventAdminParam.setUsers(users);
+        eventAdminParam.setStates(states);
+        eventAdminParam.setCategories(categories);
+        eventAdminParam.setRangeStart(rangeStart);
+        eventAdminParam.setRangeEnd(rangeEnd);
+        eventAdminParam.setFrom(from);
+        eventAdminParam.setSize(size);
+
+        return eventService.getEventsAdmin(eventAdminParam);
+    }
+
+    @PatchMapping("/admin/events/{eventId}")
+    @ResponseStatus(HttpStatus.OK)
+    public EventFullDto updateEventAdmin(@PathVariable long eventId, @Valid @RequestBody UpdateEventAdminRequest updateEventAdmin) {
+        return eventService.updateEventAdmin(eventId, updateEventAdmin);
+    }
+
+    @GetMapping("/events")
+    @ResponseStatus(HttpStatus.OK)
+    public List<EventShortDto> getEventsPublic(
+            @RequestParam(required = false) String text,
+            @RequestParam(required = false) List<Long> categories,
+            @RequestParam(required = false) Boolean paid,
+            @RequestParam(required = false) @DateTimeFormat(pattern = DATE_FORMAT) LocalDateTime rangeStart,
+            @RequestParam(required = false) @DateTimeFormat(pattern = DATE_FORMAT) LocalDateTime rangeEnd,
+            @RequestParam(required = false, defaultValue = "false") Boolean onlyAvailable,
+            @RequestParam(required = false) String sort,
+            @RequestParam(required = false, defaultValue = "0") Integer from,
+            @RequestParam(required = false, defaultValue = "10") Integer size,
+            HttpServletRequest request) {
+
+        EventUserParam eventUserParam = new EventUserParam();
+        eventUserParam.setText(text);
+        eventUserParam.setCategories(categories);
+        eventUserParam.setPaid(paid);
+        eventUserParam.setRangeStart(rangeStart);
+        eventUserParam.setRangeEnd(rangeEnd);
+        eventUserParam.setOnlyAvailable(onlyAvailable);
+        eventUserParam.setSort(sort);
+        eventUserParam.setFrom(from);
+        eventUserParam.setSize(size);
+
+        return eventService.getEventsPublic(eventUserParam, request);
+    }
+
+    @GetMapping("/events/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public EventFullDto getEventByIdPublic(@PathVariable long id, HttpServletRequest request) {
+        return eventService.getEventByIdPublic(id, request);
+    }
 }
