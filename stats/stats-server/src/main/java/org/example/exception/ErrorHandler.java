ff6302afbd2eea2552bcd9ec61c376c71ea14b33
Dmitrii Derendyaev <dds_stalker@mail.ru>
2024-05-05 16:24:04 +0400

    fix: исправить валидацию Missing parameters

--- stats/stats-server/src/main/java/org/example/exception/ErrorHandler.java
+++ stats/stats-server/src/main/java/org/example/exception/ErrorHandler.java
@@ -2,6 +2,7 @@ package org.example.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,6 +17,14 @@ public class ErrorHandler {
         log.warn(e.getMessage(), e);
         return new ErrorResponse("Validation for statistic failed: ", e.getMessage());
     }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handlerStatsValidationException(final MissingServletRequestParameterException e) {
+        log.warn(e.getMessage(), e);
+        return new ErrorResponse("Missing parameters: ", e.getMessage());
+    }
+
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
